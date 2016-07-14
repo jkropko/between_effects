8f49323054c24fb26e4058546d3a3dcde7933f47
bob_kubinec <rmk7xy@virginia.edu>
2016-07-14 21:59:57 +0200

    Revert "model results for VDEM models"
    This reverts commit 5c405f26bc8d912e4f83130c89b454d86ff42d97.

--- r_scripts/vdem_demonstration.R
+++ r_scripts/vdem_demonstration.R
@@ -5,67 +5,47 @@ require(haven)
 require(dplyr)
 require(magrittr)
 require(sandwich)
-require(tibble)
 require(ggplot2)
-require(stringr)
 
 #Run once, use a faster R version
 #full_dataset <- read_dta("C:/Users/bobku/Box Sync/Between Effects/V-DEM/Country_Year_V-Dem_other_STATA_v6.1/V-Dem-DS-CY+Others-v6.1.dta")
 #full_dataset <- saveRDS(object = full_dataset,"data/vdem_full.rds")
-# vdem_pos <- data.table::fread('data/polyarchy.csv')
 
 full_dataset <- readRDS('data/vdem_full.rds')
 
 full_dataset$year_factor <- as.factor(full_dataset$year)
 
-
-# Load posterior estimates of the V-DEM indices
-
-vdem_pos <- as.tbl(as.data.frame(readRDS('data/vdem_pos.rds')))
-vdem_pos$V1 <- NULL
-
-# combined posterior estimates with posterior uncertainty
-merged_data <- left_join(full_dataset,vdem_pos,by=c('country_text_id','year'))
-to_analyze <- merged_data[,c('e_migdppcln','year_factor','country_name')]
-
 models <- c('Within','Between','Two-way')
-varnames <- paste0('V',2:900)
-
-over_posterior <- function(x,y) {
-  to_analyze$v2x_polyarchy <- merged_data[[x]]
-  model1 <- lm(formula = y,data = to_analyze)
-  # Use the sandwich estimator to adjust variances
-  # Need to drop coefs that come out as NA
-  # This happens with the country of the Republic of Vietnam in the within-between model (model 5)
-  # The sandwich estimator will automatically drop NA coefficients from the VCOV matrix, causing an error
-  # With MASS
-  coefs <- coef(model1)[!is.na(coef(model1))]
-  sds <- vcovHC(model1,type='HC0')
-  
-  samples <- MASS::mvrnorm(mu=coefs,Sigma=sds)
-  return(samples)
-  }
 
 # Within effect
 
-model1 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + country_name)
-output_model1 <- data_frame(betas=row.names(model1),coef=apply(model1,1,mean),
-                        sd=apply(model1,1,sd))
+model1 <- lm(full_dataset,formula = v2x_polyarchy ~ e_migdppcln + country_name)
+coef_model1 <- coef(model1)['e_migdppcln']
 # Between effect
 
-model2 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + year_factor)
-output_model2 <- data_frame(betas=row.names(model2),coef=apply(model2,1,mean),
-                            sd=apply(model2,1,sd))
+model2 <- lm(full_dataset,formula = v2x_polyarchy ~ e_migdppcln + year_factor)
+coef_model2 <- coef(model2)['e_migdppcln']
 # 2-way FX
 
-model3 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + year_factor + country_name)
-output_model3 <- data_frame(betas=row.names(model3),coef=apply(model3,1,mean),
-                            sd=apply(model3,1,sd))
+model3 <- lm(full_dataset,formula = v2x_polyarchy ~ e_migdppcln + year_factor + country_name)
+coef_model3 <- coef(model3)['e_migdppcln']
 
-
+all_coefs <- c(coef_model1,coef_model2,coef_model3)
+names(all_coefs) <- models
 # All three are statistically significant, the within effect is the largest
 
+# Use sandwich estimator on the covariance matrices
+
+true_sds <- list(vcovHC(model1,type="HC0"),vcovHC(model2,type='HC0'),vcovHC(model3,type='HC0'))
+true_sds <- lapply(true_sds,function(x) sqrt(diag(x)))
+true_sds <- sapply(true_sds,function(x) x['e_migdppcln'])
+names(true_sds) <- models
+
 # Combine estimates
+
+estimates <- tbl_df(data.frame(Beta_GDP=all_coefs,SE=true_sds,Upper_CI=all_coefs + (1.96*true_sds),Lower_CI=all_coefs - (1.96*true_sds)))
+
+print(estimates)
 
 
 # Try a few more exotic models
@@ -73,57 +53,15 @@ output_model3 <- data_frame(betas=row.names(model3),coef=apply(model3,1,mean),
 # Between effect that varies over time
 # Note that this model implicitly accounts for autocorrelation
 
-model4 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + year_factor + e_migdppcln*year_factor)
-output_model4 <- data_frame(betas=row.names(model4),coef=apply(model4,1,mean),
-                            sd=apply(model4,1,sd))
-
-
+model4 <- lm(full_dataset,formula = v2x_polyarchy ~ e_migdppcln + year_factor + e_migdppcln*year_factor)
+coef_model4 <- coef(model4)[grepl("e_migdppcln",names(coef(model4)))]
+sds_model4 <- vcovHC(model4,type='HC0')
+sds_model4 <- sqrt(diag(sds_model4))
+sds_model4 <- sds_model4[grepl("e_migdppcln",names(sds_model4))]
 # Within effect that varies between
 
-model5 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + country_name + e_migdppcln*country_name)
-output_model5 <- data_frame(betas=row.names(model5),coef=apply(model5,1,mean),
-                            sd=apply(model5,1,sd))
-
-#Models with controls
-to_analyze <- merged_data[,c('e_migdppcln','year_factor','country_name','e_WORLD_DEM_DIFFUSE',
-                             'e_Vanhanen_studentpercent_ipo','e_Vanhanen_familyfarm_ipo',
-                             'e_cap_share_unequal','e_Total_Fuel_Income_PC',
-                             'e_Vanhanen_familyfarm_ipo','e_miferrat','e_Civil_War','e_Vanhanen_urban_ipo')]
-
-model6 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln + e_WORLD_DEM_DIFFUSE + 
-                e_cap_share_unequal + e_Total_Fuel_Income_PC  + country_name)
-
-output_model6 <- data_frame(betas=row.names(model6),coef=apply(model6,1,mean),
-                            sd=apply(model6,1,sd))
-
-model7 <- sapply(varnames,over_posterior,v2x_polyarchy ~ e_migdppcln +
-                    e_cap_share_unequal + e_Total_Fuel_Income_PC  + 
-                   e_miferrat + e_Civil_War + year_factor)
-output_model7 <- data_frame(betas=row.names(model7),coef=apply(model7,1,mean),
-                            sd=apply(model7,1,sd))
-
-
-# Calculate interaction effects and plot
-
-model5 <- readRDS("data/model5.rds") %>% t %>% as.data.frame %>% tbl_df
-countries <- select(model5,matches(":country"))
-int_matrix <- tbl_df(lapply(countries,function(x) x + model5[['e_migdppcln']]))
-results <- data_frame(Coef=sapply(int_matrix,mean), SD = sapply(int_matrix,sd))
-results$variables <- str_extract(colnames(countries),'[A-Z].+')
-results <- mutate(results,upper=Coef + 1.96*SD,lower=Coef - 1.96*SD)
-
-ggplot(results,aes(x=Coef,y=reorder(variables,Coef))) + geom_point() + ggnetwork::theme_blank() + geom_text(aes(label=variables),hjust=-1,check_overlap=TRUE) +
-  geom_vline(xintercept=0) + geom_errorbarh(aes(xmin=lower,xmax=upper))
-
-model5 <- readRDS("data/model5.rds") %>% t %>% as.data.frame %>% tbl_df
-countries <- select(model5,matches(":country"))
-int_matrix <- tbl_df(lapply(countries,function(x) x + model5[['e_migdppcln']]))
-results <- data_frame(Coef=sapply(int_matrix,mean), SD = sapply(int_matrix,sd))
-results$variables <- str_extract(colnames(countries),'[A-Z].+')
-results <- mutate(results,upper=Coef + 1.96*SD,lower=Coef - 1.96*SD)
-
-ggplot(results,aes(x=Coef,y=reorder(variables,Coef))) + geom_point() + ggnetwork::theme_blank() + geom_text(aes(label=variables),hjust=-1,check_overlap=TRUE) +
-  geom_vline(xintercept=0) + geom_errorbarh(aes(xmin=lower,xmax=upper))
-
-
- 
+model5 <- lm(full_dataset,formula = v2x_polyarchy ~ e_migdppcln + country_name + e_migdppcln*country_name)
+coef_model5 <- coef(model5)[grepl("e_migdppcln",names(coef(model5)))]
+sds_model5 <- vcovHC(model5,type='HC0')
+sds_model5 <- sqrt(diag(sds_model5))
+sds_model5 <- sds_model5[grepl("e_migdppcln",names(sds_model5))]
